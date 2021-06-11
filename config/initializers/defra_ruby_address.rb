798ad07dce763ffddfed4b255de2f5c749bd1a7f
irisfaraway <iris.faraway@gmail.com>
2021-06-11 14:42:04 +0100

    update timeout

--- config/initializers/defra_ruby_address.rb
+++ config/initializers/defra_ruby_address.rb
@@ -6,4 +6,5 @@ DefraRuby::Address.configure do |configuration|
   configuration.host = ENV["ADDRESSBASE_URL"] || "http://localhost:9002"
   configuration.key = ENV["ADDRESS_FACADE_CLIENT_KEY"]
   configuration.client_id = ENV["ADDRESS_FACADE_CLIENT_ID"]
+  configuration.timeout = 10
 end
